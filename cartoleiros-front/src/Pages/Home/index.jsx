0055de0619bd871fd951b6093dc0643981f6f726
lucas <lucasmlunag@gmail.com>
2026-05-06 21:47:53 -0300

    terceiro commit, cada dia mas longe de alcançar o objetivo

--- cartoleiros-front/src/Pages/Home/index.jsx
+++ cartoleiros-front/src/Pages/Home/index.jsx
@@ -1,47 +1,34 @@
-import { useEffect, useState, useRef } from 'react';
-import { useNavigate} from 'react-router-dom';
-import './style.css'
+
+import { useEffect } from 'react';
 import api from '../../services/api'
+import './HomeStyle.css' 
 
 function Home() {
-  const navigate = useNavigate();
 
-const inputNome = useRef();
-const inputSenha = useRef();
-
-  async function  postUsers() {
-    try {
-      const response = await api.post('/login-adm', {
-        nome: inputNome.current.value,
-        senha: inputSenha.current.value
-      });
-      
-      if (response.data.success) {
-        navigate("/pagina-inicial");
+  useEffect(() => {
+    async function getUsers() {
+      try {
+        const response = await api.get('/cadastro');
+        console.log(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar usuários:", error);
       }
-
-    } catch (error) {
-      console.error("Erro ao realizar login:", error);
     }
+
+    getUsers();
+  }, []);
+
+  function buttonEnterLogin() {
+    window.location.href = "/login-adm";
   }
 
-  
   return (
-  <div>
-    <div className='titulo'>
-        <h1>cartoleiros</h1>
-    </div>
+    <div className='home'>
+      <h1>Bem-vindo à Página Inicial</h1>
 
-    <div className='formulario'>
-        <h2>Login ADM</h2>
-            <form action="">
-                <input type="text" placeholder="Usuário" ref={inputNome} />
-                <input type="password" placeholder="Senha" ref={inputSenha} />
-                <button type="button" onClick={postUsers}>Entrar</button> 
-            </form>
+      <button onClick={buttonEnterLogin}>Entrar como ADM</button>
     </div>
-  </div>  
-    )
+  )
 }
 
-export default Home
+export default Home;
--- cartoleiros-front/src/main.jsx
+++ cartoleiros-front/src/main.jsx
@@ -1,13 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import Home from './Pages/Home'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+
+import Home from "./Pages/Home";
+import Login from "./Pages/Login/Login";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pagina-inicial" element={<Home />} />
+        <Route path="/login-adm" element={<Login />} />
+      </Routes>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
